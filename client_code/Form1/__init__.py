99be76403012287a597118077163d616a5547aa1
ian@anvil.works <ian@anvil.works>
2023-09-02 20:38:09 +0000

    Created Form1.RowTemplate1; Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -5,13 +5,10 @@ import anvil.server
 class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
+    self.pods = [p['metadata'] for p in anvil.server.call('list_pods')['items']]
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
-
-  def button_1_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    pods = anvil.server.call('list_pods')
-    for p in pods['items']:
-      print(f"{p['metadata']['namespace']}: {p['metadata']['name']}")
+    # for p in pods['items']:
+    #   print(f"{p['metadata']['namespace']}: {p['metadata']['name']}")
 
