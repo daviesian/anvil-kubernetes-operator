af818692dbdebcb86ca6ed7e94b961881a86a4f3
ian@anvil.works <ian@anvil.works>
2023-09-03 11:42:22 +0000

    Edited ServerModule1 and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -5,15 +5,19 @@ import anvil.server
 class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
-    self.pods = [p['metadata'] for p in anvil.server.call('list_pods')['items']]
+    self.pods = []
     self.init_components(**properties)
-
-    # Any code you write here will run before the form opens.
-    # for p in pods['items']:
-    #   print(f"{p['metadata']['namespace']}: {p['metadata']['name']}")
 
   def button_1_click(self, **event_args):
     """This method is called when the button is clicked"""
-    anvil.server.call("deploy_anvil")
+    anvil.server.call("create_cluster")
 
+  def timer_1_tick(self, **event_args):
+    """This method is called Every [interval] seconds. Does not trigger if [interval] is 0."""
+    self.update_pods()
+
+  def update_pods(self):
+    with anvil.server.no_loading_indicator:
+      self.pods = [p['metadata'] for p in anvil.server.call('list_pods')['items']]
+    self.refresh_data_bindings()
 
--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -9,34 +9,17 @@ def list_pods():
   return client.CoreV1Api().list_pod_for_all_namespaces().to_dict()
 
 @anvil.server.callable
-def deploy_anvil():
+def create_cluster():
   config.load_config()
-  api_instance = client.AppsV1Api()
-  api_instance.create_namespaced_deployment(
-    namespace="default",
-    body=client.V1Deployment(
-      metadata=client.V1ObjectMeta(
-        name="platform-server"
-      ),
-      spec=client.V1DeploymentSpec(
-        selector=client.V1LabelSelector(
-          match_labels={"anvil":"platform-server"}
-        ), 
-        template=client.V1PodTemplateSpec(
-          metadata=client.V1ObjectMeta(
-            name="busybox",
-            labels={"anvil": "platform-server"},
-          ),
-          spec=client.V1PodSpec(
-            image_pull_secrets=[client.V1LocalObjectReference(name="anvil-registry")],
-            containers=[
-              client.V1Container(
-                name="platform-server",
-                image="anvil.works:4455/on-site/anvil-platform-server"
-              ),
-            ]
-          )
-        )
-      )
-    )
-  )
+
+  client.CustomObjectsApi().create_namespaced_custom_object(
+      group="anvil.works",
+      version="v1",
+      namespace="default",
+      plural="anvilclusters",
+      body={
+      "apiVersion": "anvil.works/v1",
+      "kind": "AnvilCluster",
+      "metadata": { "name": "cluster-1" },
+      "spec": {},
+  })
